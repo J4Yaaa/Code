106c97f74fc6846b6b6667f9fa1084a38f379ae3
J4Yaaa <J4Yaaa@163.com>
2018-07-17 18:50:54 +0800

    python相关联系4

---
+++ python4/add.py
@@ -0,0 +1,12 @@
+def Add(x,y):
+    return x + y
+
+def Print():
+    print globals()
+
+# 可以利用 __name__ 测试当前是不是入口函数
+# if __name__ == '__main__':
+#     print 'add : __main__'
+
+
+print 'hello world'
